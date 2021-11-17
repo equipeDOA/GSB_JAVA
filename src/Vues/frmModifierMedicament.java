c13323b88c952660468f27d9e132592a723c9fe3
Fellous <fellousdavid95200@gmail.com>
2021-11-17 15:10:01 +0100

    finalisation de la structure du projet

--- src/Vues/frmModifierMedicament.java
+++ src/Vues/frmModifierMedicament.java
@@ -9,12 +9,12 @@ package Vues;
  *
  * @author fello
  */
-public class frmTest extends javax.swing.JFrame {
+public class frmModifierMedicament extends javax.swing.JFrame {
 
     /**
-     * Creates new form frmTest
+     * Creates new form frmModifierMedicament
      */
-    public frmTest() {
+    public frmModifierMedicament() {
         initComponents();
     }
 
@@ -60,20 +60,20 @@ public class frmTest extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(frmTest.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmModifierMedicament.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(frmTest.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmModifierMedicament.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(frmTest.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmModifierMedicament.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(frmTest.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmModifierMedicament.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new frmTest().setVisible(true);
+                new frmModifierMedicament().setVisible(true);
             }
         });
     }
